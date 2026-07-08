dfe76b88c710734c10fa6a0dc8f5451896cd52d8
Himanshu <hgautam2010@gmail.com>
2026-07-08 11:08:17 +0000

    preptin: export Copy List With Random Pointer: solution

---
+++ leetcode/copy-list-with-random-pointer/solution.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        map<Node*, Node*> mp;
+
+        Node* tmp = head;
+
+        while (tmp) {
+            mp[tmp] = new Node(tmp->val);
+            tmp = tmp->next;
+        }
+
+        tmp = head;
+
+        while (tmp) {
+            if (mp[tmp->next])
+                mp[tmp]->next = mp[tmp->next];
+            if (mp[tmp->random])
+                mp[tmp]->random = mp[tmp->random];
+            tmp = tmp->next;
+        }
+
+        return mp[head];
+    }
+};
