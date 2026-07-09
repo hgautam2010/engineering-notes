667ca585c29c04e36819e1a9e6aa47f93002fe4b
Himanshu <hgautam2010@gmail.com>
2026-07-09 09:06:13 +0000

    preptin: export Duplicate Zeros: solution

--- leetcode/duplicate-zeros/solution.cpp
+++ leetcode/duplicate-zeros/solution.cpp
@@ -2,28 +2,43 @@ class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
         vector<int> result;
-        int j = 0, i = 0;
+        int right = 0, left = 0, zeros = 0;
         int n = arr.size();
 
-        for (auto x : arr) {
-            i++;
-            if (x == 0) {
-                j++;
-                if (j == n) break;
-            }
-            j++;
-            if (j == n) break;
-        }
-        i--;
+        for (left = 0; left < n && right < n; left++) {
+            int x = arr[left];
 
-        for (int k = n - 1; i >= 0 && k >= 0; i--) {
-            if (arr[i] == 0) {
-                arr[k] = 0; k--;
-                if (k >= 0)
-                arr[k] = 0; k--;
+            if (x == 0) {
+                right += 2;
             } else {
-                arr[k] = arr[i];
-                k--;
+                right += 1;
+            }
+        }
+
+        if (left == right) {
+            return;
+        } else {
+            left--;
+            if (arr[left] == 0) {
+                if (right - n == 2) {
+                    left--;
+                } else if (right - n == 1) {
+                    zeros = 1;
+                }
+            }
+        }
+
+        for (int k = n - 1; left >= 0; left--) {
+            if (arr[left] == 0) {
+                if (zeros == 0) {
+                    arr[k--] = 0;
+                    arr[k--] = 0;
+                } else {
+                    arr[k--] = 0;
+                    zeros = 0;
+                }
+            } else {
+                arr[k--] = arr[left];
             }
         }
     }
