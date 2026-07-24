cea58669a28dfb885d5ab847d58e5da8524deb51
Himanshu <hgautam2010@gmail.com>
2026-07-24 06:00:26 +0000

    preptin: export 3514. Number of Unique XOR Triplets II: solution

--- dsa-algo-practice/leetcode/number-of-unique-xor-triplets-ii/solution.cpp
+++ dsa-algo-practice/leetcode/number-of-unique-xor-triplets-ii/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
+    static constexpr int MMAX = (1 << 11);
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int n = nums.size();
-        unordered_set<int> st;
-        unordered_set<int> res;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                st.insert(nums[i] ^ nums[j]);
+        array<bool, MMAX> uniq{};
+        array<bool, MMAX> ans{};
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j <= i; j++) {
+                uniq[nums[i] ^ nums[j]] = 1;
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            for (auto a : st) {
-                res.insert(a ^ nums[i]);
+        for (int i = 0; i < MMAX; i++) {
+            if (uniq[i]) {
+                for (auto n : nums) {
+                    ans[i ^ n] = true;
+                }
             }
         }
-
-        return res.size();
-    }
+        int val = 0;
+        for (int i = 0; i < MMAX; i++) {
+            val += ans[i];
+        }
+        return val;
+    }   
 };
